de122bc19e1f185180fb8b004950c9056039fc9f
ncalero <ncalero@gmail.com>
2017-07-05 23:12:06 +0200

    More tests

--- C#/Grades/Grades.Tests/Types/ReferenceTypeTests.cs
+++ C#/Grades/Grades.Tests/Types/ReferenceTypeTests.cs
@@ -7,9 +7,46 @@ using System.Threading.Tasks;
 
 namespace Grades.Tests.Types
 {
+   
+
     [TestClass]
     public class TypeTests
     {
+        [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+        }
+
+        [TestMethod]
+        public void UppercaseString()
+        {
+            string name = "scott";
+            name = name.ToUpper();
+
+            Assert.AreEqual("SCOTT", name);
+
+        }
+
+        [TestMethod]
+        public void AddDaysToDateTime()
+        {
+            DateTime date = new DateTime(2015, 1, 1);
+            date = date.AddDays(1);
+
+            Assert.AreEqual(2, date.Day);
+        }
+
         [TestMethod]
         public void ValueTypesPassByValue()
         {
