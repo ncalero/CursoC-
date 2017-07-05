dfe78c9d32bb96b5b1bf397b22d21fbc8d187544
ncalero <ncalero@gmail.com>
2017-07-06 00:11:19 +0200

    Delegates & Events

--- C#/Grades/Grades/Program.cs
+++ C#/Grades/Grades/Program.cs
@@ -11,16 +11,48 @@ namespace Grades
         static void Main(string[] args)
         {
             GradeBook book = new GradeBook();
+
+            //book.NameChanged += new NameChangedDelegate(onNameChanged);
+            //book.NameChanged += new NameChangedDelegate(onNameChanged2);
+            //book.NameChanged += new NameChangedDelegate(onNameChanged2);
+            book.NameChanged += (onNameChanged);
+            
+            book.Name = "Scott's Grade Book";
+            book.Name = "Joes's Grade Book";
+
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
-            GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.LowestGrade);
+            
 
-            Console.ReadLine();
+            GradeStatistics stats = book.ComputeStatistics();
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+            Console.WriteLine(book.Name);
+            //Console.WriteLine(stats.AverageGrade);
+            //Console.WriteLine(stats.HighestGrade);
+            //Console.WriteLine(stats.LowestGrade);
+            
+        }
+
+        static void onNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+        }
+
+       
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine("{0}: {1:F2}", description, result);
+            Console.WriteLine($"{description}: {result:F2}", description, result);
         }
     }
 }
