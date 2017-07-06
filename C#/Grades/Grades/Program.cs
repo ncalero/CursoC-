5b28c8677ad978dd0f55e40a25a8dee87406adb1
ncalero <nuria.calero@gmail.com>
2017-07-06 14:37:51 +0200

    Contro Flow Part1

--- C#/Grades/Grades/Program.cs
+++ C#/Grades/Grades/Program.cs
@@ -15,10 +15,10 @@ namespace Grades
             //book.NameChanged += new NameChangedDelegate(onNameChanged);
             //book.NameChanged += new NameChangedDelegate(onNameChanged2);
             //book.NameChanged += new NameChangedDelegate(onNameChanged2);
-            book.NameChanged += (onNameChanged);
+            //book.NameChanged += (onNameChanged);
             
-            book.Name = "Scott's Grade Book";
-            book.Name = "Joes's Grade Book";
+            //book.Name = "Scott's Grade Book";
+            //book.Name = "Joes's Grade Book";
 
             book.AddGrade(91);
             book.AddGrade(89.5f);
@@ -30,19 +30,25 @@ namespace Grades
             WriteResult("Average", stats.AverageGrade);
             WriteResult("Highest", (int)stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
-            Console.WriteLine(book.Name);
+            WriteResult("Grade", stats.Description);
+            
+            //Console.WriteLine(book.Name);
             //Console.WriteLine(stats.AverageGrade);
             //Console.WriteLine(stats.HighestGrade);
             //Console.WriteLine(stats.LowestGrade);
             
         }
 
-        static void onNameChanged(object sender, NameChangedEventArgs args)
-        {
-            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
-        }
+        //static void onNameChanged(object sender, NameChangedEventArgs args)
+        //{
+        //    Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+        //}
 
-       
+
+        static void WriteResult(string description, string result)
+        {
+            Console.WriteLine("{0}: {1}", description, result);        
+        }
 
         static void WriteResult(string description, int result)
         {
@@ -52,7 +58,7 @@ namespace Grades
         static void WriteResult(string description, float result)
         {
             Console.WriteLine("{0}: {1:F2}", description, result);
-            Console.WriteLine($"{description}: {result:F2}", description, result);
+            //Console.WriteLine($"{description}: {result:F2}", description, result);
         }
     }
 }
