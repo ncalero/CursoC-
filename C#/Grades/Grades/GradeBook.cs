fce6d6fd733414668b66dccbce2b7caee9459937
ncalero <ncalero@gmail.com>
2017-07-06 23:42:47 +0200

    Control Flow Part 2

--- C#/Grades/Grades/GradeBook.cs
+++ C#/Grades/Grades/GradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,6 +30,14 @@ namespace Grades
             return stats;
         }
 
+        internal void WriteGrades(TextWriter destination)
+        {
+            for (int i = grades.Count; i > 0; i--)
+            {
+                destination.WriteLine(grades[i - 1]);
+            }
+        }
+
         public void AddGrade(float grade)
         {
             grades.Add(grade);
@@ -42,18 +51,20 @@ namespace Grades
             }
             set
             {
-                if (!String.IsNullOrEmpty(value))
+                if(String.IsNullOrEmpty(value))
                 {
-                    if (_name != value)
-                    {
-                        NameChangedEventArgs args = new NameChangedEventArgs();
-                        args.ExistingName = _name;
-                        args.NewName = value;
-                        
-                        NameChanged(this, args);
-                    }
-                    _name = value;
+                    throw new ArgumentNullException("Name cannot be null or empty");
                 }
+                if (_name != value && NameChanged !=null)
+                {
+                    NameChangedEventArgs args = new NameChangedEventArgs();
+                    args.ExistingName = _name;
+                    args.NewName = value;
+
+                    NameChanged(this, args);
+                }
+                _name = value;
+
             }
         }
 
--- C#/Grades/Grades/Program.cs
+++ C#/Grades/Grades/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,14 @@ namespace Grades
         {
             GradeBook book = new GradeBook();
 
+            GetBookName(book);
+
+
             //book.NameChanged += new NameChangedDelegate(onNameChanged);
             //book.NameChanged += new NameChangedDelegate(onNameChanged2);
             //book.NameChanged += new NameChangedDelegate(onNameChanged2);
             //book.NameChanged += (onNameChanged);
-            
+
             //book.Name = "Scott's Grade Book";
             //book.Name = "Joes's Grade Book";
 
@@ -24,19 +28,40 @@ namespace Grades
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
-            
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+
+            }
 
             GradeStatistics stats = book.ComputeStatistics();
             WriteResult("Average", stats.AverageGrade);
             WriteResult("Highest", (int)stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
             WriteResult("Grade", stats.Description);
-            
+
             //Console.WriteLine(book.Name);
             //Console.WriteLine(stats.AverageGrade);
             //Console.WriteLine(stats.HighestGrade);
             //Console.WriteLine(stats.LowestGrade);
-            
+
+        }
+
+        private static void GetBookName(GradeBook book)
+        {
+            try
+            {
+                Console.WriteLine("Enter a name");
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
         }
 
         //static void onNameChanged(object sender, NameChangedEventArgs args)
