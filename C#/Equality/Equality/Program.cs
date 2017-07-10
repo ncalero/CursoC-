d49df0458f7f5c4413081bab3f33ed85bd5f1df5
ncalero <ncalero@gmail.com>
2017-07-10 23:52:36 +0200

    Equality

---
+++ C#/Equality/Equality/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Food banana = new Food("banana");
+            Food banana2 = new Food("banana");
+            Food chocolate = new Food("chocolate");
+
+            Console.WriteLine(banana.Equals(chocolate));
+            Console.WriteLine(banana.Equals(banana2));
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+}
